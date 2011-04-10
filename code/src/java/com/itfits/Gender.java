cdc26e03e636c0ad4a3b8c104b8a80f5701ee7a8
Denise Ichinco <ichinco@gmail.com>
2011-04-10 14:15:05 -0400

    changed show around, also to show images

---
+++ code/src/java/com/itfits/Gender.java
@@ -0,0 +1,14 @@
+package com.itfits;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: denise
+ * Date: 4/10/11
+ * Time: 1:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Gender {
+    MALE,
+    FEMALE,
+    UNISEX
+}
