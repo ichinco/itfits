1a047b4bf32a5c72773c2f947a733c0472f73cba
evilwire <evilwire@gmail.com>
2011-04-24 11:29:15 -0400

    - added mysql connection by installing connector/j plugin
     - KF

--- code/grails-app/conf/DataSource.groovy
+++ code/grails-app/conf/DataSource.groovy
@@ -1,32 +1,37 @@
 dataSource {
     pooled = true
-    driverClassName = "org.hsqldb.jdbcDriver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    dbCreate = "create-drop"
+
+    username = "grailsadmin"
+    password = '$u13pol0nium'
+
+    url = "jdbc:mysql://localhost:3306/test"
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
-    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+    cache.provider_class = "net.sf.ehcache.hibernate.EhCacheProvider"
 }
+
 // environment specific settings
 environments {
     development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:hsqldb:mem:devDB"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:hsqldb:mem:testDb"
-        }
-    }
-    production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
-        }
-    }
+		    dataSource {
+			      dbCreate = "update" // one of 'create', 'create-drop','update'
+			      url = "jdbc:mysql://localhost:3306/test"
+		    }
+	  }
+	  test {
+		    dataSource {
+			      dbCreate = "update"
+			      url = "jdbc:mysql://localhost:3306/test"
+		    }
+	  }
+	  production {
+		    dataSource {
+			      dbCreate = "update"
+			      url = "jdbc:mysql://localhost:3306/code_production"
+		    }
+	  }
 }
