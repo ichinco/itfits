b9976697f0db72afbdf1f6018a386178d139a72b
evilwire <evilwire@gmail.com>
2011-06-11 11:00:13 -0400

    - added the Outfits domain class
     - KF

---
+++ code/grails-app/domain/Outfits.groovy
@@ -0,0 +1,33 @@
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: glassfin
+ * Date: 6/11/11
+ * Time: 10:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class Outfits {
+    String style
+    String price
+    String description
+
+    Date dateCreated
+    Date lastUpdated
+
+    static hasMany = [
+        reviews:Review,
+        ratings:ClothingRating,
+        tags:Tag,
+        colors:Color,
+        materials:ClothingMaterial,
+        votes:VoteRecord,
+        imageUrls:String
+    ]
+
+    static constraints = {
+        style nullable:true
+        description nullable:true
+        imageUrls nullable:true
+        price nullable:true
+    }
+}
